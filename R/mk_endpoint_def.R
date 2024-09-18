f10bc281ac5f658c495ccf5010e1f2fd13effaac
CINO (Christian Haargaard Olsen) <cino@novonordisk.com>
2024-09-18 12:49:51 +0200

    Update endpoint definition and command for staging
    Update the endpoint definition to filter records to be included.
    Change the command used to stage the pipeline, and show how to
    use tar_make for running it.

--- R/mk_endpoint_def.R
+++ R/mk_endpoint_def.R
@@ -11,6 +11,7 @@ mk_endpoint_def <- function() {
         stratify_by = list(c("AGEGR2")),
         data_prepare = mk_adae_template,
         endpoint_label = "Any adverse events - test data - on-treatment - safety analysis set",
+        endpoint_filter = "!is.na(AEDECOD)",
         custom_pop_filter = "TRT01A %in% c('Placebo', 'Xanomeline High Dose')",
         stat_by_strata_by_trt = list("n_subev" = c(chefStats::n_subj_event)),
         stat_by_strata_across_trt = list("RR" = c(chefStats::RR)),
--- run.R
+++ run.R
@@ -1,4 +1,9 @@
 ## When you are starting up your R-sessions and want to continue your work
 
 library(chef)
-chef::use_pipeline("cdisc_pilot")
+
+## Select the pipeline targets is working on
+chef::stage_pipeline("cdisc_pilot")
+
+## Run the pipeline
+tar_make()
