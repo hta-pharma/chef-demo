e567954fda3ebdff0f0f474fde52cbba1e35ea7d
Christian Haargaard Olsen <cino@novonordisk.com>
2024-09-24 15:29:40 +0200

    Add files created by setup

---
+++ R/packages.R
@@ -0,0 +1,7 @@
+### If you need any packages for you functions, put the library calls here, NOT
+### in the individual scripts
+library(chef)
+library(targets)
+library(tarchetypes)
+library(magrittr)
+library(data.table)
