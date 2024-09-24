233a02261e82d60b6664c7901b3e034de413a0ff
Christian Haargaard Olsen <cino@novonordisk.com>
2024-09-24 15:35:08 +0200

    Add packages to be used in data pipeline

---
+++ R/packages.R
@@ -0,0 +1,9 @@
+### If you need any packages for you functions, put the library calls here, NOT
+### in the individual scripts
+library(chef)
+library(targets)
+library(tarchetypes)
+library(magrittr)
+library(data.table)
+library(tidyr) 
+library(haven) ## For loading xpt files
