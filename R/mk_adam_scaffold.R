b15b4b32cd472eac76cebda1bbef4f283ee2679f
Christian Haargaard Olsen <christianhaargaard@gmail.com>
2024-10-29 14:14:29 +0000

    Update data function to match example

--- R/mk_adam_scaffold.R
+++ R/mk_adam_scaffold.R
@@ -1,13 +1,15 @@
-mk_adae_template <- function(study_metadata){
+mk_adae <- function(study_metadata){
     adae <- data.table::as.data.table(haven::read_xpt("data/adae.xpt"))
     adsl <- data.table::as.data.table(haven::read_xpt("data/adsl.xpt"))
 
     ## Make a subgroup:
-    adsl[, AGEGR2 := data.table::fcase(AGE < 65, "AGE < 65",
+    adsl[, AGEGR := data.table::fcase(AGE < 65, "AGE < 65",
                                        AGE >= 65, "AGE >= 65",
                                        default = NA)]
     
     ## Merge adsl and adae
     adae_out <- merge(adsl, adae[, c(setdiff(names(adae), names(adsl)), "USUBJID"), with =
                                                                                 F], by = "USUBJID", all = TRUE)
-}
+    ## Filter to only have two treatments
+    adae_out[TRT01A %in% c('Placebo', 'Xanomeline High Dose'),]
+    }
