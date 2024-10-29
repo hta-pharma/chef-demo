adc7cf9bdeb4fb3f7fa8ea289b89b56193a841a7
Christian Haargaard Olsen <christianhaargaard@gmail.com>
2024-10-29 14:14:55 +0000

    Simplify endpoint definition

--- R/mk_endpoint_def.R
+++ R/mk_endpoint_def.R
@@ -1,20 +1,19 @@
 mk_endpoint_def <- function() {
-    ## Put your calls to mk_endpoint_str() here. For multiple endpoints, rbind several calls to mk_endpoint_str()
     mk_endpoint_str(
-        study_metadata = list(),
-        pop_var = "SAFFL",
-        pop_value = "Y",
-        treatment_var = "TRT01A",
-        treatment_refval = "Xanomeline High Dose",
-        period_var = "SAFFL",
-        period_value = "Y",
-        stratify_by = list(c("AGEGR2")),
-        data_prepare = mk_adae_template,
-        endpoint_label = "Any adverse events - test data - on-treatment - safety analysis set",
-        endpoint_filter = "!is.na(AEDECOD)",
-        custom_pop_filter = "TRT01A %in% c('Placebo', 'Xanomeline High Dose')",
-        stat_by_strata_by_trt = list("n_subev" = c(chefStats::n_subj_event)),
-        stat_by_strata_across_trt = list("RR" = c(chefStats::RR)),
-        stat_across_strata_across_trt = list('pvalI' = c(chefStats::p_val_interaction))
+      data_prepare = mk_adae,
+      treatment_var = "TRT01A",
+      treatment_refval = "Xanomeline High Dose",
+      pop_var = "SAFFL",
+      pop_value = "Y",
+      stratify_by = list(c("SEX","AGEGR","RACE")),
+      stat_by_strata_by_trt = list(
+        chefStats::n_subj,
+        chefStats::n_subj_event,
+        demo_n_events),
+      stat_by_strata_across_trt = list(
+        chefStats::RR,
+        chefStats::OR,
+        chefStats::p_val),
+      endpoint_filter = "!is.na(AEDECOD)"
     )
 }
