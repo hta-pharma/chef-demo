e23cdc7c90b99713b3008c773281b221c9fad449
Christian Haargaard Olsen <christianhaargaard@gmail.com>
2024-10-30 08:50:52 +0000

    Renaming files

--- R/data_preparation.R
+++ R/data_preparation.R
@@ -12,4 +12,20 @@ mk_adae <- function(study_metadata){
                                                                                 F], by = "USUBJID", all = TRUE)
     ## Filter to only have two treatments
     adae_out[TRT01A %in% c('Placebo', 'Xanomeline High Dose'),]
-    }
+}
+
+mk_adae_low <- function(study_metadata){
+  adae <- data.table::as.data.table(haven::read_xpt("data/adae.xpt"))
+  adsl <- data.table::as.data.table(haven::read_xpt("data/adsl.xpt"))
+  
+  ## Make a subgroup:
+  adsl[, AGEGR := data.table::fcase(AGE < 65, "AGE < 65",
+                                    AGE >= 65, "AGE >= 65",
+                                    default = NA)]
+  
+  ## Merge adsl and adae
+  adae_out <- merge(adsl, adae[, c(setdiff(names(adae), names(adsl)), "USUBJID"), with =
+                                 F], by = "USUBJID", all = TRUE)
+  ## Filter to only have two treatments
+  adae_out[TRT01A %in% c('Placebo', 'Xanomeline Low Dose'),]
+}
--- R/endpoint_definitions.R
+++ R/endpoint_definitions.R
@@ -1,5 +1,5 @@
 mk_endpoint_def <- function() {
-    mk_endpoint_str(
+    rbind(mk_endpoint_str(
       data_prepare = mk_adae,
       treatment_var = "TRT01A",
       treatment_refval = "Xanomeline High Dose",
@@ -15,5 +15,23 @@ mk_endpoint_def <- function() {
         chefStats::OR,
         chefStats::p_val),
       endpoint_filter = "!is.na(AEDECOD)"
+    ),
+    mk_endpoint_str(
+    data_prepare = mk_adae_low,
+    treatment_var = "TRT01A",
+    treatment_refval = "Xanomeline Low Dose",
+    pop_var = "SAFFL",
+    pop_value = "Y",
+    stratify_by = list(c("SEX","AGEGR","RACE")),
+    stat_by_strata_by_trt = list(
+      chefStats::n_subj,
+      chefStats::n_subj_event),
+    stat_by_strata_across_trt = list(
+      chefStats::RR,
+      chefStats::OR,
+      chefStats::p_val),
+    endpoint_filter = "!is.na(AEDECOD)"
+  )
+
     )
 }
